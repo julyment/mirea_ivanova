896a34fd3d5d02cc45c1cef8e61e94b064d980c4
julyment <juliva1370@gmail.com>
2025-11-28 10:58:28 +0300

    Практическая 7

--- src/task_1/Developer.java
+++ src/task_1/Developer.java
@@ -3,56 +3,34 @@ package task_1;
 import java.util.Date;
 
 public class Developer {
-    private String name;
-    private String surname;
-    private Date receive_task;
-    private Date end_task;
+    private String surname;      // фамилия разработчика
+    private String receiveDate;  // дата/время получения (как строка)
+    private Date submitDate;     // дата/время сдачи (как Date)
 
-    public Developer(String name, String surname, Date receive_task, Date end_task){
-        this.name = name;
+    public Developer(String surname, String receiveDate) {
         this.surname = surname;
-        this.receive_task = receive_task;
-        this.end_task =  end_task;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.receiveDate = receiveDate;
     }
 
     public String getSurname() {
         return surname;
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    public String getReceiveDate() {
+        return receiveDate;
     }
 
-    public Date getReceive_task() {
-        return receive_task;
+    public Date getSubmitDate() {
+        return submitDate;
     }
 
-    public void setReceive_task(Date receive_task) {
-        this.receive_task = receive_task;
+    // фиксируем дату/время сдачи
+    public void markSubmitted() {
+        this.submitDate = new Date(); // текущие дата и время
     }
 
-    public Date getEnd_task() {
-        return end_task;
-    }
-
-    public void setEnd_task(Date end_task) {
-        this.end_task = end_task;
-    }
-
-    @Override
-    public String toString() {
-        return "Разаботчик\n" + "Имя: " + name + "\nФамилия" + surname
-                + "\nВыдача задания: " + receive_task + "\nДедлайн: " + end_task;
-    }
+}
 
     // проверка receive end через after/before
     // как устанавливать дату выдачи задания? с консоли? в каком виде?
-}
+
--- src/task_1/Testing.java
+++ src/task_1/Testing.java
@@ -1,17 +1,11 @@
 package task_1;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Testing {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();;
-        Scanner sc = new Scanner(System.in);
-
-        while(true){
-
-        }
-
 
     }
 }
